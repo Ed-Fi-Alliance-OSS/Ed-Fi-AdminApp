34e96169c8616b6492d29192a5733bfa881ad689
Juan Agudelo <105452282+jagudelo-gap@users.noreply.github.com>
2025-12-04 17:19:46 -0500

    [AC-366] Create Release Workflow in GitHub Actions (#12)
    * Add Initial work flow files
    
    * Remove VITE config values from front end docker file
    
    * Update npm registry link
    
    * Refactor Docker publish workflow and update image names
    
    * Update docker image build steps
    
    * update sbom create
    
    * simplify SBOM  and provenance creation steps
    
    * change push branch
    
    * Add npm package validation
    
    * Fix review comments
    
    * Variable name change
    
    * move config.ts to config folder
    
    * Simplify docker files
    
    * Add default values to config.ts
    
    * Simplify Prepare package versions
    
    * Fix local dev app run
    
    ---------
    
    Co-authored-by: CSR2017 <suganya@imanisoftware.com>

--- packages/fe/vite.config.ts
+++ packages/fe/vite.config.ts
@@ -1,48 +1,56 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { visualizer } from 'rollup-plugin-visualizer';
 
-export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/',
-  root: __dirname,
-  build: {
-    outDir: '../../dist/packages/fe',
-    emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
+export default defineConfig(({ mode }) => {
+  // Load VITE_* envs from .env files and process.env
+  const env = loadEnv(mode, process.cwd());
+
+  // Base path for the app
+  const base = env.VITE_BASE_PATH
+  return {
+    base,
+    root: __dirname,
+    build: {
+      outDir: '../../dist/packages/fe',
+      emptyOutDir: true,
+      reportCompressedSize: true,
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
     },
-  },
-  cacheDir: '../../node_modules/.vite/fe',
+    cacheDir: '../../node_modules/.vite/fe',
 
-  server: {
-    port: 4200,
-    host: true,
-    fs: {
-      allow: ['../../node_modules/@fontsource'],
+    server: {
+      port: 4200,
+      host: true,
+      fs: {
+        // Allow serving files from the app dir and repo root
+        allow: [__dirname, '../../','../../node_modules/@fontsource'],
+      },
+      headers: {
+        'X-Content-Type-Options': 'nosniff',
+        'X-Frame-Options': 'deny',
+        //Content Security Policy cannot be set locally, but should be in production
+      },
     },
-    headers: {
-      'X-Content-Type-Options': 'nosniff',
-      'X-Frame-Options': 'deny',
-      //Content Security Policy cannot be set locally, but should be in production
+
+    preview: {
+      port: 4300,
+      host: 'localhost',
     },
-  },
 
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
+    plugins: [react(), nxViteTsPaths(), visualizer()],
 
-  plugins: [react(), nxViteTsPaths(), visualizer()],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [
-  //    viteTsConfigPaths({
-  //      root: '../../',
-  //    }),
-  //  ],
-  // },
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [
+    //    viteTsConfigPaths({
+    //      root: '../../',
+    //    }),
+    //  ],
+    // },
+  };
 });
