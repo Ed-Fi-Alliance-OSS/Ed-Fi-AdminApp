bd06460664d6fad5ea65a51f0fd25fcde62f7dd3
Jorge David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2026-07-01 09:02:51 -0600

    Progress on the Pages

--- packages/fe/src/app/Pages/Ods/OdssPage.tsx
+++ packages/fe/src/app/Pages/Ods/OdssPage.tsx
@@ -1,9 +1,53 @@
-import { PageActions, PageTemplate, SbaaTableAllInOne } from '@edanalytics/common-ui';
+import { Icons, PageActions, PageTemplate, SbaaTableAllInOne, TableRowActions } from '@edanalytics/common-ui';
 import { useQuery } from '@tanstack/react-query';
 import { odsQueries } from '../../api/queries/queries';
 import { useTeamEdfiTenantNavContextLoaded } from '../../helpers';
-import { NameCell } from './NameCell';
+import { CellContext } from '@tanstack/react-table';
 import { useOdssActions } from './useOdssActions';
+import { Badge, HStack, Link } from '@chakra-ui/react';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import { GetOdsDto } from '@edanalytics/models';
+import { odsStatusDisplayMap } from './Utils';
+
+const useOdsRowActions = (ods: GetOdsDto) => {
+  const { teamId, edfiTenant } = useTeamEdfiTenantNavContextLoaded();
+  const navigate = useNavigate();
+  const to = `/as/${teamId}/sb-environments/${edfiTenant.sbEnvironmentId}/edfi-tenants/${edfiTenant.id}/odss/${ods.id}/`;
+  return {
+    View: {
+      icon: Icons.View,
+      text: 'View',
+      title: 'View ' + ods.displayName,
+      to,
+      onClick: () => navigate(to),
+    },
+    Delete: {
+      icon: Icons.Delete,
+      text: 'Delete',
+      title: 'Delete ODS',
+      confirmBody: 'This will permanently delete the ODS.',
+      confirm: true,
+      onClick: () => {},
+    },
+  };
+};
+
+const NameCell = (info: CellContext<GetOdsDto, unknown>) => {
+  const { teamId, edfiTenant } = useTeamEdfiTenantNavContextLoaded();
+  const { id, displayName } = info.row.original;
+  const to = `/as/${teamId}/sb-environments/${edfiTenant.sbEnvironmentId}/edfi-tenants/${edfiTenant.id}/odss/${id}/`;
+  const actions = useOdsRowActions(info.row.original);
+  return (
+    <HStack justify="space-between">
+      <Link as="span">
+        <RouterLink title="Go to ODS" to={to}>
+          {displayName}
+        </RouterLink>
+      </Link>
+      <TableRowActions actions={actions} />
+    </HStack>
+  );
+};
 
 export const OdssPage = () => {
   const actions = useOdssActions();
@@ -27,10 +71,12 @@ export const OdssTable = () => {
     <SbaaTableAllInOne
       data={Object.values(odss?.data || {})}
       columns={[
-        {
-          accessorKey: 'displayName',
-          cell: NameCell,
-          header: 'Name',
+        { accessorKey: 'displayName', cell: NameCell, header: 'Name' },
+        { accessorKey: 'instanceType', header: 'Type' },
+        { accessorKey: 'status', header: 'Status', cell: (info) => {
+            const { label, colorScheme } = info.row.original.status != null ? odsStatusDisplayMap[info.row.original.status] : odsStatusDisplayMap['null'];
+            return <Badge colorScheme={colorScheme}>{label}</Badge>;
+          },
         },
       ]}
     />
--- packages/fe/src/app/Pages/Ods/ViewOds.tsx
+++ packages/fe/src/app/Pages/Ods/ViewOds.tsx
@@ -1,3 +1,4 @@
+import { Badge } from '@chakra-ui/react';
 import { AttributeContainer, AttributesGrid, ContentSection } from '@edanalytics/common-ui';
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
@@ -5,6 +6,7 @@ import { edfiTenantQueries, odsQueries } from '../../api';
 import { queryFromEntity } from '../../api/queries/builder';
 import { useTeamEdfiTenantNavContextLoaded } from '../../helpers';
 import { EdfiTenantLink, SbEnvironmentLink } from '../../routes';
+import { odsStatusDisplayMap } from './Utils';
 
 export const ViewOds = () => {
   const params = useParams() as {
@@ -26,6 +28,8 @@ export const ViewOds = () => {
     })
   );
 
+  const { label, colorScheme } = ods ? (ods.status != null ? odsStatusDisplayMap[ods.status] : odsStatusDisplayMap['null']) : odsStatusDisplayMap['null'];
+
   return ods ? (
     <ContentSection>
       <AttributesGrid>
@@ -35,6 +39,12 @@ export const ViewOds = () => {
         <AttributeContainer label="Tenant">
           <EdfiTenantLink id={ods.edfiTenantId} query={edfiTenants} />
         </AttributeContainer>
+        <AttributeContainer label="Name">{ods.displayName}</AttributeContainer>
+        <AttributeContainer label="Type">{ods.instanceType}</AttributeContainer>
+        <AttributeContainer label="Status">
+          <Badge colorScheme={colorScheme}>{label}</Badge>
+        </AttributeContainer>
+        <AttributeContainer label="Database Name">{ods.databaseName}</AttributeContainer>
       </AttributesGrid>
     </ContentSection>
   ) : null;
