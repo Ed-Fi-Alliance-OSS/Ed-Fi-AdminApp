85a701a1b4e320e8761d5633328a8c8fd355ce5d
Jorge David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2026-07-31 13:38:00 -0600

    Fix based on comments

--- packages/api/src/database/migrations/mssql/1785365966591-RenameDbInstanceIdToInstanceManageId.ts
+++ packages/api/src/database/migrations/mssql/1785365966591-RenameDbInstanceIdToInstanceManageId.ts
@@ -4,10 +4,10 @@ export class RenameDbInstanceIdToInstanceManageId1785365966591 implements Migrat
   name = 'RenameDbInstanceIdToInstanceManageId1785365966591';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`EXEC sp_rename 'ods.dbInstanceId', 'instanceManageId', 'COLUMN'`);
+    await queryRunner.query(`EXEC sp_rename '[ods].[dbInstanceId]', 'instanceManageId', 'COLUMN'`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`EXEC sp_rename 'ods.instanceManageId', 'dbInstanceId', 'COLUMN'`);
+    await queryRunner.query(`EXEC sp_rename '[ods].[instanceManageId]', 'dbInstanceId', 'COLUMN'`);
   }
 }
