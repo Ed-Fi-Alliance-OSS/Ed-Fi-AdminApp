c131cfecadad2a5e2cce5dc5e7e6cbe8dc39961f
Jesus Pardo <jpardo@growthaccelerationpartners.com>
2026-08-27 10:09:03 -0400

    Fix fe unit test

--- packages/fe/jest.config.ts
+++ packages/fe/jest.config.ts
@@ -5,6 +5,7 @@ export default {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
+  transformIgnorePatterns: ['node_modules/(?!@tanstack/match-sorter-utils|remove-accents)'],
   setupFiles: ['<rootDir>/jest.setup.js'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/packages/fe',
